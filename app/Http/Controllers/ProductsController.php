2826e04073d6beb3512bec8f2cc4738c6af03088
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-06-08 02:03:29 -0500

    .

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -85,6 +85,7 @@ class ProductsController extends Controller
         // do {
         $code = substr(str_shuffle($permitted_chars), 0, 10);
         $products = Product::where('code', $code)->get();
+        echo json_encode($products);
         // var_dump($products);
         //     if ($products) {
         //         $codeUnique = false;
