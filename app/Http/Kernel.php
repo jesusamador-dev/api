5c233026a8db1fbc3348763e29fb6995ec9e6b08
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-06-07 20:54:09 -0500

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.role' => \App\Http\Middleware\RoleAuthorization::class,
-
     ];
 }
